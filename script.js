1bd1494076e04ca4321a9d8fa2df7cf5f59936f7
Luxlupus <ispiljak@gmail.com>
2024-01-05 21:56:17 +0100

    Updated math functions/logic

--- script.js
+++ script.js
@@ -3,66 +3,95 @@ let b = 0;
 let result = null;
 let operator = null;
 
+//creating varibles for display
+const displayNum = document.getElementById('displayNum');
+const activeDisplay = document.createElement("span");
+
 //main math function declarations
 function add(a, b) {
-    result = a + b;
-    console.log(result);
+    result =  a + b;
+    console.log("Result is", result);
+    console.log("Result is", typeof(result));
+    result = +result.toFixed(7);
     return result;
 }
 function subtract(a, b) {
     result = a - b;
-    console.log(result);
+    console.log("Result is", result);
+    console.log("Result is", typeof(result));
+    result = +result.toFixed(7);
     return result;
 }
 function multiply(a, b) {
     result = a * b;
-    console.log(result);
+    console.log("Result is", result);
+    console.log("Result is", typeof(result));
+    result = +result.toFixed(7);
     return result;       
 }
 function divide(a, b) {
     result = a / b;
-    console.log(result);
+    console.log("Result is", result);
+    console.log("Result is", typeof(result));
+    result = +result.toFixed(7);
     return result;
 }
 //for calling the math function depending on operator sign
 function operate(operator, a, b) {
-    if (operator === "+")
-        {
-            add(a, b);
+    if (operator === "+"){
+        add(a, b);
+        newCalc(a, result);
+    }
+    else if(operator === "-"){
+        subtract(a, b);
+        newCalc(a, result);
+    }
+    else if(operator === "*"){
+        multiply(a, b);
+        newCalc(a, result);
+    }
+    else if(operator === "/"){
+        divide(a, b);
+        newCalc(a, result);
         }
-    else if (operator === "-")
-        {
-            subtract(a, b);
-        }
-    else if (operator === "*")
-        {
-            multiply(a, b);
-        }
-    else if (operator === "/")
-        {
-            divide(a, b);
-        }
+    else if(operator === null){
+        init();
+        newCalc(a, result);
+    }
 }
-//creating varibles to display
-const displayNum = document.getElementById('displayNum');
-const activeDisplay = document.createElement("span");
+
+//assigning result values if user decides to continue calculating with acquired result
+function newCalc(a, result) {
+a = result;
+console.log("Result has become new a:", a); }
 
 //assign value before operation to variable a, after operation to variable b. 
 const numBtns = document.getElementsByClassName('btnNum');
 for (const element of numBtns) {
     element.addEventListener('click', () => {
-        
         if(operator === null){ 
-        a = Number(a + element.value);
-        console.log("Value of a is:", a);
-        activeDisplay.textContent = a;
-        displayNum.appendChild(activeDisplay);
+            a = Number(a + element.value);
+            console.log("Value of a is:", a);
+            console.log(typeof(a));
+            activeDisplay.textContent = a;
+            displayNum.appendChild(activeDisplay);
         }
-        else {
-        b = Number(b + element.value);
-        console.log("Value of b is:", b);
-        activeDisplay.textContent = b;
-        displayNum.appendChild(activeDisplay);
+        else if(operator !== null && b === 0){
+            b = Number(b + element.value);
+            console.log("Value of b is:", b);
+            console.log(typeof(b));
+            activeDisplay.textContent = b;
+            displayNum.appendChild(activeDisplay);
+        }
+//if user starts new calculation without clearing
+        else if(result !== null){
+            a = 0;
+            b = 0;
+            a = Number(a + element.value);
+            console.log("Value of a is:", a);
+            console.log(typeof(a));
+            activeDisplay.textContent = a;
+            displayNum.appendChild(activeDisplay);
         }
     }) 
 }  
@@ -70,30 +99,66 @@ for (const element of numBtns) {
 const operatorBtns = document.getElementsByClassName('btnOperator');
 for (const element of operatorBtns) {
     element.addEventListener('click', () => {
-        operator = element.value;
-        console.log("Operator is:",operator);
-        activeDisplay.textContent = operator;
-        displayNum.appendChild(activeDisplay);
+        if (operator === null) {
+            activateDecPoint();
+            operator = element.value;
+            console.log("Operator is:",operator);
+            activeDisplay.textContent = operator;
+            displayNum.appendChild(activeDisplay);
+        }
+        //assign operator sign for next operation and calculate if the equal sign is not clicked
+        else if(operator !== null) {
+            activateDecPoint();
+            operate(operator, a, b);
+            a = result;
+            console.log("Value of new a is:", a);
+            b = 0;
+            console.log("Value of new b is:", b);
+            operator = element.value;
+            console.log("New operator is:",operator);
+            activeDisplay.textContent = operator;
+            displayNum.appendChild(activeDisplay);
+        }
     });
 }
+//adding decimal point
 const decPoint = document.getElementById('decimalPoint');
 decPoint.addEventListener('click', () => {
-    decimalPoint = decPoint.value;
-    console.log(decimalPoint) });
+    if(operator === null){
+        decPoint.disabled = true;
+        a = a + ".";
+        activeDisplay.textContent = a;
+        displayNum.appendChild(activeDisplay);
+    
+    }
+    else {
+        decPoint.disabled = true;
+        b = b + ".";
+        activeDisplay.textContent = b;
+        displayNum.appendChild(activeDisplay);
+    }
+});
 
+//reactivate decimal point button for next input
+function activateDecPoint() {
+    decPoint.disabled = false;
+}
 //calling corresponding math function when "=" clicked
 const equals = document.getElementById('equals');
 equals.addEventListener('click', () => { 
     operate(operator, a, b)
     activeDisplay.textContent = result;
-    displayNum.appendChild(activeDisplay);});
+    displayNum.appendChild(activeDisplay);
+    decPoint.disabled = true;
+});
 
+//clearing the display when C clicked and reset variables
 const clearBtn = document.getElementById('clearBtn');
-
-//clearing the display when C clicked and reseting variables
 clearBtn.addEventListener('click', () => {
     displayNum.innerHTML = ""
-    init(); });
+    init();
+    activateDecPoint();
+});
 
 //getting back initial values of global variables    
 function init() {
